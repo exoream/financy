ae6fa7a97ebd7bfa20352679f803f3ce7fa8fca9
exoream <shiigetsu1@gmail.com>
2025-06-03 00:02:24 +0800

    first commit

--- prisma/migrations/20250529165016_increase_refresh_token_length/migration.sql
+++ prisma/migrations/20250529165016_increase_refresh_token_length/migration.sql
@@ -1,3 +1,2 @@
--- Active: 1748777384682@@103.163.138.212@3306@absens11_fikry
--- AlterTable
-ALTER TABLE `user` MODIFY `refreshToken` TEXT NULL;
+
+ALTER TABLE `User` MODIFY `refreshToken` TEXT NULL;
